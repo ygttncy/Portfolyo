42461a13d2e6e4a8c524d54c8f2236be0404072b
ygttncy <yigit03yigit@gmail.com>
2025-03-08 13:14:53 +0300

    Add New Certificates

--- src/sections/Certificates/Certificates.jsx
+++ src/sections/Certificates/Certificates.jsx
@@ -2,7 +2,7 @@ import styles from './CertificatesStyles.module.css';
 import certificate1 from '../../assets/Advanced.jpg';
 import certificate2 from '../../assets/Beginner.jpg';
 import certificate3 from '../../assets/Intermediate.jpg';
-
+import certificate4 from '../../assets/backend.jpg';
 function Certificates() {
   return (
     <section id="certificates" className={styles.container}>
@@ -10,15 +10,19 @@ function Certificates() {
       <div className={styles.certificatesContainer}>
         <div className={styles.certificateCard}>
           <img src={certificate2} alt="Certificate 1" />
-          <p>Beginner Developer Certificate</p>
+          <p>Beginner Froented Developer Certificate</p>
         </div>
         <div className={styles.certificateCard}>
           <img src={certificate3} alt="Certificate 2" />
-          <p>Intermediate Developer Certificate</p>
+          <p>Intermediate Froented Developer Certificate</p>
         </div>
         <div className={styles.certificateCard}>
           <img src={certificate1} alt="Certificate 3" />
-          <p>Advanced Developer Certificate</p>
+          <p>Advanced Froented Developer Certificate</p>
+        </div>
+        <div className={styles.certificateCard}>
+          <img src={certificate4} alt="Certificate 4" />
+          <p>Beginner Backend Developer Certificate</p>
         </div>
       </div>
     </section>
